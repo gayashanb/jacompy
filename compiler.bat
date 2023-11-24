6975700de89e8e7c4dab854c05a313ea57eb8260
Gayashan B <33860784+gayashanb@users.noreply.github.com>
2023-11-24 20:29:43 +0530

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -1,6 +1,10 @@
 @echo off
-echo Input the name of Java program with (.java) extention
+echo ===================
+echo J4V4 C0MP1L3R v0.1
+echo ===================
+echo Enter the name of Java program with (.java) extention
 set /p resp="Name: "
-echo "Compiling & Executing.."
+echo Compiling and Executing..
 javac %resp%
-%resp%
+echo Printing output..
+java %resp%
